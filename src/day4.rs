b8d725bc4d7361df268c1af9aaaa5814ff8825d6
river-000 <94073590+river-000@users.noreply.github.com>
2021-12-04 13:21:18 +0000

    [day4] parse grids

--- src/day4.rs
+++ src/day4.rs
@@ -2,15 +2,38 @@ use std::fs;
 
 //use advent_of_code::parse_number;
 use advent_of_code::parse_commasep_numbers;
+use advent_of_code::parse_whitespacesep_numbers;
+use advent_of_code::parse_grid_numbers;
+
+pub fn parse_day4_grid(i: &str) -> nom::IResult<&str, Vec<Vec<u64>>> {
+    let (i,_) = nom::character::complete::newline(i)?;
+    let (i,_) = nom::character::complete::newline(i)?;
+    let (i,g) = parse_grid_numbers(i)?;
+
+    Ok((i,g))
+}
+
+pub fn parse_day4_grids(i: &str) -> nom::IResult<&str, Vec<Vec<Vec<u64>>>> {
+    nom::multi::many1(parse_day4_grid)(i)
+}
+
+pub fn parse_day4(i: &str) -> nom::IResult<&str, (Vec<u64>, Vec<Vec<Vec<u64>>>)> {
+    let (i,ns) = parse_commasep_numbers(i)?;
+    let (i,gs) = parse_day4_grids(i)?;
+
+    Ok((i,(ns,gs)))
+}
+  
 
 pub fn parse(filename: &str) -> Result<Vec<u64>, ()> {
     let text = fs::read_to_string(filename).unwrap();
 
-    match parse_commasep_numbers(&text) {
-        Ok((_,x)) => {
-            println!("{:?}", x);
-            //Err(())
-            Ok(x)
+    match parse_day4(&text) {
+        Ok((_,(ns,gs))) => {
+            println!("{:?}", ns);
+            println!("{:?}", gs);
+            Err(())
+            //Ok(x)
         }
         Err(_) => Err(())
     }
--- src/lib.rs
+++ src/lib.rs
@@ -25,5 +25,14 @@ pub fn parse_number(i: &str) -> nom::IResult<&str, u64> {
 }
 
 pub fn parse_commasep_numbers(i: &str) -> nom::IResult<&str, Vec<u64>> {
-    nom::multi::separated_list(nom::bytes::complete::tag(","), parse_number)(i)
+  nom::multi::separated_list1(nom::bytes::complete::tag(","), parse_number)(i)
+}
+
+pub fn parse_whitespacesep_numbers(i: &str) -> nom::IResult<&str, Vec<u64>> {
+  let (i,_) = nom::character::complete::space0(i)?;
+  nom::multi::separated_list1(nom::character::complete::space1, parse_number)(i)
+}
+
+pub fn parse_grid_numbers(i: &str) -> nom::IResult<&str, Vec<Vec<u64>>> {
+  nom::multi::separated_list1(nom::character::complete::newline, parse_whitespacesep_numbers)(i)
 }
