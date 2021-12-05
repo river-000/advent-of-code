ecb8d1751a6bb5187260bbbf5a475764d2e654f8
river-000 <94073590+river-000@users.noreply.github.com>
2021-12-05 12:46:57 +0000

    time each day

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,21 @@ mod day3;
 mod day4;
 mod day5;
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn time_it(f: fn() -> ()) {
+    let start = SystemTime::now();
+    f();
+    let stop = SystemTime::now();
+    let since_the_epoch = stop.duration_since(start).expect("Time went backwards");
+    println!("{:?}", since_the_epoch);
+    println!("")
+}
+
 fn main() {
-    day1::day();
-    day2::day();
-    day3::day();
-    day4::day();
-    day5::day();
+    time_it(day1::day);
+    time_it(day2::day);
+    time_it(day3::day);
+    time_it(day4::day);
+    time_it(day5::day);
 }
